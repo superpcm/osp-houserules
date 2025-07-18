4ee6223cf3e80f85733c6d52fb02f15b203fd3b9
superpcm <paul.c.mackney@gmail.com>
2025-07-18 11:31:45 -0400

    Working Actor dialog.

--- dist/ose.js
+++ dist/ose.js
@@ -44,6 +44,16 @@ class OspActorSheetMonster extends foundry.appv1.sheets.ActorSheet {
 
 class OspActor extends Actor {
   /** @override */
+  get displayName() {
+    return "Actor";
+  }
+
+  /** @override */
+  get displayNamePlural() {
+    return "Actors";
+  }
+
+  /** @override */
   prepareData() {
     super.prepareData();
     // Add any system-specific actor prep here
@@ -58,17 +68,46 @@ Hooks.once("init", () => {
   foundry.documents.collections.Actors.unregisterSheet("core", foundry.appv1.sheets.ActorSheet);
   foundry.documents.collections.Actors.registerSheet("osp-houserules", OspActorSheetCharacter, {
     types: ["character"],
-    makeDefault: true
+    makeDefault: true,
+    label: game.i18n.localize("OSP.Actor.Type.character")
   });
   foundry.documents.collections.Actors.registerSheet("osp-houserules", OspActorSheetMonster, {
     types: ["monster"],
-    makeDefault: true
+    makeDefault: true,
+    label: game.i18n.localize("OSP.Actor.Type.monster")
   });
-
   CONFIG.Actor.typeLabels = {
-    character: "Character",
-    monster: "Monster"
+    character: game.i18n.localize("OSP.Actor.Type.character"),
+    monster: game.i18n.localize("OSP.Actor.Type.monster")
   };
+
+  Hooks.on("renderActorConfig", (app, html, data) => {
+    const $html = $(html);
+    const $select = $html.find('select[name="type"]');
+    const $submit = $html.find('button[type="submit"]');
+
+    // Insert a placeholder option if not present
+    if ($select.find('option[value=""]').length === 0) {
+      $select.prepend(
+        `<option value="" disabled>${game.i18n.localize("OSP.ChooseType")}</option>`
+      );
+      $select.val(""); // Set the select to the placeholder
+    }
+
+    // Localize type options
+    $select.find('option[value="character"]').text(game.i18n.localize("OSP.Actor.Type.character"));
+    $select.find('option[value="monster"]').text(game.i18n.localize("OSP.Actor.Type.monster"));
+
+    // Disable submit if no type is selected
+    $submit.prop("disabled", $select.val() === "");
+
+    // Enable submit when a valid type is chosen
+    $select.on("change", function () {
+      $submit.prop("disabled", $select.val() === "");
+    });
+
+    $html.find('input[name="name"]').val("Enter name");
+  });
 
   console.log("OSP Debug: Actor sheets registered successfully");
 });
--- src/module/actor/actor.js
+++ src/module/actor/actor.js
@@ -1,5 +1,15 @@
 export class OspActor extends Actor {
   /** @override */
+  get displayName() {
+    return "Actor";
+  }
+
+  /** @override */
+  get displayNamePlural() {
+    return "Actors";
+  }
+
+  /** @override */
   prepareData() {
     super.prepareData();
     // Add any system-specific actor prep here
